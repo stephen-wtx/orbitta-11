4e880a801d674ef7f332ab69b0768bf5919d84e3
stephen-wtx <stefeeneugenio@gmail.com>
2026-01-01 03:55:12 +0200

    video

--- assets/js/data.js
+++ assets/js/data.js
@@ -41,13 +41,41 @@ const ferramentasImagem = {
   ],
 
   "converter-formatos": [
+    { nome: "FreeConvert", url: "https://freeconvert.com" },
     { nome: "CloudConvert", url: "https://cloudconvert.com" },
     { nome: "iLoveIMG", url: "https://www.iloveimg.com" }
   ],
 
   "comprimir-imagens": [
     { nome: "TinyPNG", url: "https://tinypng.com" },
-    { nome: "CompressJPEG", url: "https://compressjpeg.com" }
+    { nome: "CompressJPEG", url: "https://compressjpeg.com" },
+    { nome: "Compressor.io", url: "https://compressor.io"}
+  ]
+};
+
+const ferramentasVideo = {
+  "comprimir-video": [
+    { nome: "Clipchamp", url: "https://www.clipchamp.com" },
+    { nome: "VEED.io", url: "https://www.veed.io" },
+    { nome: "Online UniConverter", url: "https://www.media.io" }
+  ],
+
+  "converter-formatos": [
+    { nome: "CloudConvert", url: "https://cloudconvert.com" },
+    { nome: "Online Video Converter", url: "https://www.onlinevideoconverter.com" },
+    { nome: "Zamzar", url: "https://www.zamzar.com" }
+  ],
+
+  "editar-video": [
+    { nome: "WeVideo", url: "https://www.wevideo.com" },
+    { nome: "InVideo", url: "https://invideo.io" },
+    { nome: "Kapwing", url: "https://www.kapwing.com" }
+  ],
+
+  "gerar-legendas": [
+    { nome: "Flixier", url: "https://flixier.com" },
+    { nome: "Kapwing", url: "https://www.kapwing.com/subtitles" },
+    { nome: "Adobe Express", url: "https://www.adobe.com/express/feature/video/add-caption" }
   ]
 };
 
@@ -75,7 +103,54 @@ const dados = {
         { nome: "Upscale.media", url: "https://www.upscale.media" },
         { nome: "Cotout.pro", url: "https://www.cutout.pro"}
       ]
+    },
+
+    "converter-formatos": {
+      titulo: "Converter formatos",
+      ferramentas: [
+        { nome: "CloudConvert", url: "https://cloudconvert.com" },
+        { nome: "iLoveIMG", url: "https://www.iloveimg.com" },
+        { nome: "FreeConvert", url: "https://freeconvert.com" }
+      ]
+    },
+
+    "comprimir-imagens": {
+      titulo: "Comprimir imagens",
+      ferramentas: [
+        { nome: "TinyPNG", url: "https://tinypng.com" },
+        { nome: "CompressJPEG", url: "https://compressjpeg.com" },
+        { nome: "Compressor.io", url: "https://compressor.io" }
+      ]
+    },
+
+
+    "comprimir-video": {
+      titulo: "Comprimir vídeo",
+      ferramentas: [
+        { nome: "Clipchamp", url: "https://www.clipchamp.com" },
+        { nome: "VEED.io", url: "https://www.veed.io" },
+        { nome: "Online UniConverter", url: "https://www.media.io" }
+      ]
+    },
+
+    "editar-video": {
+      titulo: "Editar vídeo",
+      ferramentas: [
+        { nome: "WeVideo", url: "https://www.wevideo.com" },
+        { nome: "InVideo", url: "https://invideo.io" },
+        { nome: "Kapwing", url: "https://www.kapwing.com" }
+      ]
+    },
+
+    "gerar-legendas": {
+      titulo: "Gerar legendas",
+      ferramentas: [
+        { nome: "Flixier", url: "https://flixier.com" },
+        { nome: "Kapwing", url: "https://www.kapwing.com/subtitles" },
+        { nome: "Adobe Express", url: "https://www.adobe.com/express/feature/video/add-caption" }
+      ]
     }
+    
   };
 
 
